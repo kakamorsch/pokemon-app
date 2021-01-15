d740dceba333d33aa865d9e134536b3d69be2de5
kakamorsch <kakamorsch@gmail.com>
2021-01-15 10:37:00 -0300

    setting the home route to find and list pokemons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,42 @@
 <template>
-  <div class="home">
-    <h3>Pokemon App</h3>
-    <div>
-      <li v-for="pokemon in pokemons" :key="pokemon.indexOf">
+  <div class="w-full flex justify-center">
+    <input
+      type="text"
+      placeholder="Enter Pokemon Here"
+      class="mt-10 p-2 border-blue-500 border-2"
+      v-model="text"
+    />
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div
+      class="ml-4 text-2xl text-blue-400"
+      v-for="(pokemon, idx) in filteredPokemon"
+      :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
         {{ pokemon.name }}
-      </li>
+      </router-link>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs, computed } from "vue";
 export default {
   name: "Home",
   setup() {
     const state = reactive({
       pokemons: [],
-      urlIdLookup: {}
+      urlIdLookup: {},
+      text: "",
+      filteredPokemon: computed(() => updatePokemon())
     });
+    function updatePokemon() {
+      if (!state.text) return [];
+      return state.pokemons.filter(pokemon =>
+        pokemon.name.includes(state.text)
+      );
+    }
     fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
       .then(res => res.json())
       .then(data => {
