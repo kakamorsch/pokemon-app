545fb918615f6c9518c53f0039de0055162ae965
kakamorsch <kakamorsch@gmail.com>
2021-01-15 09:22:04 -0300

    removing unused component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h3>Pokemon App</h3>
+    <div>
+      <li v-for="pokemon in pokemons" :key="pokemon.indexOf">
+        {{ pokemon.name }}
+      </li>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import { reactive, toRefs } from "vue";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {}
+    });
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+          {}
+        );
+      });
+    return { ...toRefs(state) };
   }
 };
 </script>
